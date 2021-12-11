935ab9bd23c59a45062b438597bb2127730b0c45
TheRowan <rowanaylett@rocketmail.com>
2021-12-11 23:08:26 +1100

    Added Side panel

--- lua/cl_config.lua
+++ lua/cl_config.lua
@@ -1,7 +1,22 @@
 dahveysf1 = dahveysf1 or {}
 dahveysf1.config = dahveysf1.config or {}
+dahveysf1.tabs = dahveysf1.tabs or {}
+dahveysf1.lang = dahveysf1.lang or {}
+
 -- CONFIG
 
 dahveysf1.config.DermaSkin = "Dahveyflat"
 dahveysf1.config.BgColour = Color(0, 0, 0, 166)
 dahveysf1.config.HeaderBgColour = Color(0, 0, 0, 200)
+dahveysf1.config.SideBgColour = Color(0, 0, 0, 215)
+
+-- Tabs
+
+dahveysf1.tabs["rules"]     = {order = 0, enabled = true}
+dahveysf1.tabs["commands"]  = {order = 1, enabled = true}
+dahveysf1.tabs["about"]     = {order = 2, enabled = true}
+dahveysf1.tabs["settings"]  = {order = 3, enabled = true}
+
+-- Lang
+
+
